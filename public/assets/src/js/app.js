f75902341270fc07a84f110f85085c4cdf2db47e
Gordan Dzinovic <gdzinovic@zendesk.com>
2017-04-19 17:58:57 +0200

    cleaning

--- public/assets/src/js/app.js
+++ public/assets/src/js/app.js
@@ -1,12 +1,7 @@
 // This should be the entry point of ASYNC loaded script
 // (not mandatory for early layout setting)
 
-// import Vendor, { Herb as Herbert } from '../vendors/v.js'; // ES6 baby!!!!
 import './globals.js'; // Shows how to expose globals, DO NOT DO THIS :)
+import './plugins.js';
 
 console.log('im app & im heavy weight & loaded ASYNC');
-// const v = new Vendor();
-// v.init();
-// Herbert();
-
-import './plugins.js';
